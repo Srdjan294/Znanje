44197ab4f4346270bdda0268ee4d03996040cca9
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-05 16:26:45 +0100

    unos i ispis 3 broja u obrnutom redosljedu

---
+++ eclipse/src/znanje/problem005.java
@@ -0,0 +1,23 @@
+package znanje;
+
+import java.util.Scanner;
+
+public class problem005 {
+	
+	// 1_4. Napisati program za upis i ispis tri broja.
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Unesite 3 broja");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		
+		System.out.println("Brojevi u obrnutom redosljedu su: " + c + ", " + b + " i " + a);
+		
+		input.close();
+	}
+
+}
