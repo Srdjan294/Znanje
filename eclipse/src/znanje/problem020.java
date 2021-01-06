4e08fc6233f6d68208735e83e54e0f9acba0f092
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-06 14:18:44 +0100

    korijen sume kvadrata 2 broja

---
+++ eclipse/src/znanje/problem020.java
@@ -0,0 +1,26 @@
+package znanje;
+
+import java.util.Scanner;
+
+public class problem020 {
+	
+	// 6_7. Izraèunati korijen sume kvadrata dva uèitana broja.
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Uèitaj 2 broja: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		
+		double sum = Math.pow(a, 2) + Math.pow(b, 2);
+		
+		double korijen = Math.sqrt(sum);
+		
+		System.out.println("Korijen sume kvadrata 2 uèitana broja je: " + korijen);
+		
+		input.close();
+	}
+
+}
