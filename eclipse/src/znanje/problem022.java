8e825dc5fbd000b5c87b9a428446a206c1523b01
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-01-06 14:35:31 +0100

    Korijen apsolutne vrijednosti sume kvadrata dva učitana broja

---
+++ eclipse/src/znanje/problem022.java
@@ -0,0 +1,28 @@
+package znanje;
+
+import java.util.Scanner;
+
+public class problem022 {
+	
+	// 6_9. Izracunati korijen apsolutne vrijednosti sume kvadrata dva ucitana broja.
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Uèitaj 2 broja: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		
+		double sum = Math.pow(a, 2) + Math.pow(b, 2);
+		
+		double abs = Math.abs(sum);
+		
+		double korijen = Math.sqrt(abs);
+		
+		System.out.println("Korijen apsolutne vrijednosti sume kvadrata dva uèitana broja je: " + korijen);
+		
+		input.close();
+	}
+
+}
