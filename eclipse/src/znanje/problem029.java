02d716a979d315f92141521c24f929681fc2bb92
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-02-11 17:40:48 +0100

    dijagonala kvadrata

--- eclipse/src/znanje/problem029.java
+++ eclipse/src/znanje/problem029.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class problem029 {
 	
-	// 8_4. Ispisati rastojanje izmeðu dvije upisane vrijednosti.
+	// 8_4. Ispisati udaljenost izmeðu dvije upisane vrijednosti.
 
-	// Opis programa: Rastojenje je pozitivna vrijednost zato se treba izraèunati 
-	// aplsolutna vrijednost razlike dvije vrijednosti. Za to se koristi ugraðena 
+	// Opis programa: Udaljenost je pozitivna vrijednost zato se treba izraèunati 
+	// apsolutna vrijednost razlike dvije vrijednosti. Za to se koristi ugraðena 
 	// matematièka funkcija izraèunavanja apsolutne vrijednosti Math.abs(b - a)
 	
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Unesi prvi broj: ");
 		int a = input.nextInt();
+		System.out.println("Unesi drugi broj: ");
 		int b = input.nextInt();
 		
 		int c = Math.abs(a-b);
 		
-		System.out.println("Rastojenje izmeðu "+a+" i "+b+" je "+c);
+		System.out.println("Udaljenost izmeðu "+a+" i "+b+" je "+c);
 		
 		input.close();
 	}
