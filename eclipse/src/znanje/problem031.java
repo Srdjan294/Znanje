e759b0250953269a90f681fa4fe294f0a3c6cfe7
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-02-11 17:43:56 +0100

    površina pravokutnika

---
+++ eclipse/src/znanje/problem031.java
@@ -0,0 +1,25 @@
+package znanje;
+
+import java.util.Scanner;
+
+public class problem031 {
+	
+	// 10.  Napisati program za izraèunavanje površine pravokutnika.
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Unesi duljinu stranice a pravokutnika: ");
+		int a = input.nextInt();
+		System.out.println("Unesi duljinu stranice b pravokutnika: ");
+		int b = input.nextInt();
+		
+		int p = a*b;
+		
+		System.out.println("Površina pravokutnika je: "+p);
+		
+		input.close();
+	}
+
+}
