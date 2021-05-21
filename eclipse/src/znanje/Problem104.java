a9fda350781f4ddb9dcf62e440d97289ac347a30
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-21 20:37:57 +0200

    DIV i MOD od svih kombinacija 7 sa 2

---
+++ eclipse/src/znanje/Problem104.java
@@ -0,0 +1,19 @@
+package znanje;
+
+public class Problem104 {
+	
+	// 59. Napisati program za izraèunavanje koliènika i ostatka pri dijeljenju 7 sa 2 ( DIV i MOD)
+	
+	public static void main(String[] args) {
+		
+		System.out.println("7 DIV 2: " + 7/2);
+		System.out.println("7 MOD 2: " + 7%2);
+		System.out.println("-7 DIV 2: " + -7/2);
+		System.out.println("-7 MOD 2: " + -7%2);
+		System.out.println("-7 DIV -2: " + -7/-2);
+		System.out.println("-7 MOD -2: " + -7%-2);
+		System.out.println("7 DIV -2: " + 7/-2);
+		System.out.println("7 MOD -2: " + 7%-2);
+	}
+
+}
