6ce8c2044fd2165095bc0ac345b1587b86937d69
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-06-24 21:49:08 +0200

    Generiranje dvoznamenkastog broja

--- eclipse/src/znanje/Problem126.java
+++ eclipse/src/znanje/Problem126.java
@@ -1,5 +1,7 @@
 package znanje;
 
+import java.util.Scanner;
+
 public class Problem126 {
 
 	// 110. Ispisati generirani dvoznamenkasti sluèajan broj.
@@ -7,8 +9,33 @@ public class Problem126 {
 	
 	public static void main(String[] args) {
 		
-		int broj = (int)((100 - 10) * Math.random() + 10);
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println(broj);
+		while(true) {
+		
+			int broj = (int)((100 - 10) * Math.random() + 10);
+		
+			System.out.println("Generiranje dvoznamenkastog broja:");
+			System.out.println(broj);
+			
+			System.out.println("Želite li generirati novi broj: d ili n?");
+			
+			String c = input.next();
+		
+			if(c.equalsIgnoreCase("d")) {
+				continue;
+			}
+		
+			if(c.equalsIgnoreCase("n")) {
+				break;
+			}
+		
+			while(!c.equalsIgnoreCase("d") && !c.equalsIgnoreCase("n")) {
+			System.out.println("Odaberite d za da ili n za ne.");
+			c = input.next();
+			}
+			
+		}
+		input.close();
 	}
 }
