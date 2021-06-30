ef99fc7ba980abcdc1b4ff90cc98fad6bc909bc9
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-06-30 21:50:12 +0200

    Generiranje dvoznamenkastog broja i njegovih znamenki

---
+++ eclipse/src/znanje/Problem132.java
@@ -0,0 +1,49 @@
+package znanje;
+
+import java.util.Scanner;
+
+public class Problem132 {
+
+	// 116. Napisati program koji generira dvoznamenkasti broj, ispiše ga. Zatim ispiše znamenke generiranog broja.
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+		
+			int broj = (int)((100 - 10) * Math.random() + 10);
+		
+			System.out.println("Generiranje dvoznamenkastog broja:");
+			System.out.println(broj);
+			
+			int d = broj/10;
+			int j = broj%10;
+			
+			System.out.println("Znamenke: \ndesetice: " + d + "\njedinice: " + j);
+			
+			System.out.println("Želite li ponoviti: d ili n?");
+			
+			String c = input.next();
+		
+			if(c.equalsIgnoreCase("d")) {
+				continue;
+			}
+		
+			if(c.equalsIgnoreCase("n")) {
+				break;
+			}
+		
+			while(!c.equalsIgnoreCase("d") && !c.equalsIgnoreCase("n")) {
+			System.out.println("Odaberite d za da ili n za ne.");
+			c = input.next();
+			}
+			
+			if(c.equalsIgnoreCase("n")) {
+				break;
+			}
+			
+		}
+		input.close();
+	}
+}
